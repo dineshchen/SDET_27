4cb14b99036dc82e0c7dbbf65f99cbc6d9fbb56f
Gopinath <Gopinath@DESKTOP-9C5438O>
2021-12-30 22:07:15 +0530

    my git

---
+++ SDET-27-TY/src/test/java/com/crm/practice/TestClass.java
@@ -0,0 +1,11 @@
+package com.crm.practice;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+	@Test
+	public void createFile() {
+		System.out.println("New File Created");
+	}
+}
