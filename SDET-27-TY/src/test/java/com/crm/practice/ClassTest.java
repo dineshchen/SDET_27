a40d170e968764bade2dbe6238b15343b380d782
Gopinath <Gopinath@DESKTOP-9C5438O>
2021-12-30 17:55:30 +0530

    my third git file

---
+++ SDET-27-TY/src/test/java/com/crm/practice/ClassTest.java
@@ -0,0 +1,11 @@
+package com.crm.practice;
+
+import org.testng.annotations.Test;
+
+public class ClassTest {
+
+	@Test
+	public void createHistory() {
+		System.out.println("Created sucessfully");
+	}
+}
