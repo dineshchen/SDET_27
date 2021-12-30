db01bd93867f6a85eec3a242231527cc3b5881be
Gopinath <Gopinath@DESKTOP-9C5438O>
2021-12-30 17:43:34 +0530

    testclass

---
+++ SDET-27-TY/src/test/java/com/crm/practice/TestClass.java
@@ -0,0 +1,11 @@
+package com.crm.practice;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+	@Test
+	public void createLog() {
+		System.out.println("log created successfully");
+	}
+}
